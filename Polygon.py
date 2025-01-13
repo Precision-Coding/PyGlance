dc769179f605957fb1735495d74353fb0cf0843c
therealjone <alexkjones0409@hotmail.com>
2025-01-13 21:11:08 +0000

    projection fixed + stuff

--- Polygon.py
+++ Polygon.py
@@ -16,16 +16,29 @@ class Polygon:
         self.projectedcoord3 = np.array([0, 0])
         self.display = display
 
-    def rotate(self, phi, theta):
-        rotationXMatrix = [[1, 0, 0], [0, np.cos(phi), -np.sin(phi)], [0, np.sin(phi), np.cos(phi)]]
-        rotationYMatrix = [[np.cos(theta), 0, np.sin(theta)], [0, 1, 0], [-np.sin(theta), 0, np.cos(theta)]]
-        self.coord1 = np.matmul(rotationXMatrix, np.matmul(rotationYMatrix, self.coord1))
-        self.coord2 = np.matmul(rotationXMatrix, np.matmul(rotationYMatrix, self.coord2))
-        self.coord3 = np.matmul(rotationXMatrix, np.matmul(rotationYMatrix, self.coord3))
+    def rotate(self, phi, theta, psi):
+
+        rotationXMatrix = [[1, 0, 0],
+                           [0, np.cos(phi), -np.sin(phi)],
+                           [0, np.sin(phi), np.cos(phi)]]
+
+        rotationYMatrix = [[np.cos(theta), 0, np.sin(theta)],
+                           [0, 1, 0],
+                           [-np.sin(theta), 0, np.cos(theta)]]
+
+        rotationZMatrix = [[np.cos(psi), -np.sin(psi), 0],
+                           [np.sin(psi), np.cos(psi), 0],
+                           [0, 0, 1]]
+
+        self.coord1 = np.matmul(rotationZMatrix, np.matmul(rotationXMatrix, np.matmul(rotationYMatrix, self.coord1)))
+        self.coord2 = np.matmul(rotationZMatrix, np.matmul(rotationXMatrix, np.matmul(rotationYMatrix, self.coord2)))
+        self.coord3 = np.matmul(rotationZMatrix, np.matmul(rotationXMatrix, np.matmul(rotationYMatrix, self.coord3)))
+        middlecoordinate = (self.coord1+self.coord2+self.coord3)/3
+        self.coordmid = middlecoordinate
         return self.coord1, self.coord2, self.coord3
 
     def draw(self, screen):
-        pg.draw.polygon(screen, "grey", (self.projectedcoord1, self.projectedcoord2, self.projectedcoord3))
+        pg.draw.polygon(screen, self.colour, (self.projectedcoord1, self.projectedcoord2, self.projectedcoord3))
         pg.draw.polygon(screen, "black", (self.projectedcoord1, self.projectedcoord2, self.projectedcoord3), 5)
 
     def perspective_projection(self, polygon, cameravector, camera):
--- yessirrrrrrrrr projecting.py
+++ yessirrrrrrrrr projecting.py
@@ -7,9 +7,9 @@ pg.init()
 display = Display()
 
 polygon1 = Polygon(np.array([0, 100, 0]), np.array([86, -50, 0]), np.array([-86, -50, 0]), "red", display)
-polygon2 = Polygon(np.array([0, 0, 100]), np.array([86, -50, 0]), np.array([-86, -50, 0]), "red", display)
-polygon3 = Polygon(np.array([0, 100, 0]), np.array([0, 0, 100]), np.array([-86, -50, 0]), "red", display)
-polygon4 = Polygon(np.array([0, 100, 0]), np.array([86, -50, 0]), np.array([0, 0, 100]), "red", display)
+polygon2 = Polygon(np.array([0, 0, 100]), np.array([86, -50, 0]), np.array([-86, -50, 0]), "green", display)
+polygon3 = Polygon(np.array([0, 100, 0]), np.array([0, 0, 100]), np.array([-86, -50, 0]), "blue", display)
+polygon4 = Polygon(np.array([0, 100, 0]), np.array([86, -50, 0]), np.array([0, 0, 100]), "yellow", display)
 polygons = [polygon1, polygon2, polygon3, polygon4]
 
 while display.run:
@@ -21,11 +21,17 @@ while display.run:
 
     display.clock.tick(display.FPS)
     display.screen.fill("white")
+    psi = 0.01
     phi = 0.01
-    theta = 0.01
+    theta = 1
 
+    #transforms the polygons
     for polygon in polygons:
-        polygon.rotate(phi, theta)
+        polygon.rotate(phi, theta, psi)
+    #sorts the polygons
+    polygons = sorted(polygons, key=lambda polygon: -np.linalg.norm(polygon.coordmid-display.camera))
+    #projects and draws the polygons
+    for polygon in polygons:
         polygon.perspective_projection(polygon, display.cameravector, display.camera)
         polygon.draw(display.screen)
 
