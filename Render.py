b2941f50c639d56ff5f4bf13b6fc61a109f64946
therealjone <alexkjones0409@hotmail.com>
2025-01-15 23:17:16 +0000

    added frustam culling

--- Render.py
+++ Render.py
@@ -17,12 +17,17 @@ class Render():
             polygon.is_drawn = True
             #Set coordinates based on distance from bottom and left
             horizontal_plane_coordinate_distance = np.linalg.norm(np.cross(camera.direction_vector_up, clipping_plane_coordinate-camera.left_bounding_coordinate))
-            clipping_plane_width= np.linalg.norm(np.cross(camera.direction_vector_up, camera.right_bounding_coordinate-camera.left_bounding_coordinate))
-            x_coordinate = horizontal_plane_coordinate_distance / clipping_plane_width * display.screen_width
+            horizontal_plane_coordinate_distance_R = np.linalg.norm(np.cross(camera.direction_vector_up, clipping_plane_coordinate-camera.right_bounding_coordinate))
             vertical_plane_coordinate_distance = np.linalg.norm(np.cross(camera.direction_vector_right, clipping_plane_coordinate-camera.bottom_bounding_coordinate))
+            vertical_plane_coordinate_distance_T = np.linalg.norm(np.cross(camera.direction_vector_right, clipping_plane_coordinate-camera.top_bounding_coordinate))
             clipping_plane_height = np.linalg.norm(np.cross(camera.direction_vector_right, camera.top_bounding_coordinate-camera.bottom_bounding_coordinate))
+            clipping_plane_width = np.linalg.norm(np.cross(camera.direction_vector_up, camera.right_bounding_coordinate-camera.left_bounding_coordinate))
+            if horizontal_plane_coordinate_distance > clipping_plane_width or horizontal_plane_coordinate_distance_R > clipping_plane_width:
+                polygon.is_drawn = False
+            elif vertical_plane_coordinate_distance > clipping_plane_height or vertical_plane_coordinate_distance_T > clipping_plane_height:
+                polygon.is_drawn = False
+            x_coordinate = horizontal_plane_coordinate_distance / clipping_plane_width * display.screen_width
             y_coordinate = vertical_plane_coordinate_distance / clipping_plane_height * display.screen_height
-
            #Translates angle into screen coordinates
             projection_coords.append([x_coordinate, y_coordinate])
 
@@ -32,7 +37,7 @@ class Render():
         #transforms the polygons
         for polygon in model.polygon_array:
             polygon.rotate(model.pitch, model.yaw, model.roll)
-            polygon.cos_theta = (np.dot(camera.direction_vector, polygon.normal)/np.linalg.norm(polygon.normal)/np.linalg.norm(camera.direction_vector))
+            polygon.cos_theta = np.dot(camera.direction_vector, polygon.normal)
 
         #sorts the polygons
         polygons = sorted(model.polygon_array, key=lambda polygon: -np.linalg.norm(polygon.middle_coordinate-camera.position_vector))
