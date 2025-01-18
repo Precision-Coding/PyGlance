8acb3be182fc6ed5d698cd2e451c37d020b49df9
Callum Lewis <callumlewis852@gmail.com>
2025-01-18 23:02:32 +0000

    Efficiency Improvement

--- Render.py
+++ Render.py
@@ -14,27 +14,28 @@ class Render():
             y_coordinate = transformed_coordinates[1]/-transformed_coordinates[2] * 700 + display.screen_height/2
             projection_coords.append([x_coordinate, y_coordinate])
 
-        transformed_midpoint = polygon.middle_coordinate-camera.position_vector
-        cos_theta = np.dot(transformed_midpoint, polygon.normal)/np.linalg.norm(transformed_midpoint) #cos theta = (a.b)/|a||b|
-        if cos_theta < 0:
-            polygon.colour = (abs(200*cos_theta), abs(200*cos_theta), abs(200*cos_theta))
-            polygon.is_drawn = True
-        else:
-            polygon.colour = "red"
-            polygon.is_drawn = False
-
         return (projection_coords[0], projection_coords[1], projection_coords[2])
 
+    def polygonShadingAndCulling(self, polygon, camera):
+        transformed_midpoint = polygon.middle_coordinate-camera.position_vector
+        cos_theta = np.dot(transformed_midpoint, polygon.normal)/np.linalg.norm(transformed_midpoint) #cos theta = (a.b)/|a||b|
+
+        if cos_theta < 0:
+            polygon.colour = (200* -cos_theta, 200* -cos_theta, 200* -cos_theta)
+            polygon.is_drawn = True
+        else:
+            polygon.is_drawn = False
+
     def pygameDrawModel(self, display, camera, model):
-        #transforms the polygons
+        #transforms the polygons shades and culls backfacing
         for polygon in model.polygon_array:
             polygon.rotate(model.pitch, model.yaw, model.roll)
+            self.polygonShadingAndCulling(polygon, camera)
 
         #sorts the polygons
-        polygons = sorted(model.polygon_array, key=lambda polygon: -np.linalg.norm(polygon.middle_coordinate-camera.position_vector))
+        polygons = sorted(model.polygon_array, key=lambda polygon: np.linalg.norm(polygon.middle_coordinate-camera.position_vector), reverse=True)
 
         #projects and draws the polygons
         for polygon in polygons:
-            polygon_position = self.polygonPerspectiveProjection(polygon, camera, display)
             if polygon.is_drawn:
-                pg.draw.polygon(display.screen, polygon.colour, polygon_position)
+                pg.draw.polygon(display.screen, polygon.colour, self.polygonPerspectiveProjection(polygon, camera, display))
